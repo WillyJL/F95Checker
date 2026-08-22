2f4a678b5db9216dc5405794363372d138f34e3d
WillyJL <me@willyjl.dev>
2026-08-22 17:05:51 +0200

    Detect .msi too, keep magics in their own variable

--- modules/callbacks.py
+++ modules/callbacks.py
@@ -190,10 +190,13 @@ async def _launch_exe(executable: str):
         open_webpage(exe.as_uri())
         return
 
+    windows_exe_magics = (b"MZ", b"ZM", b"\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1")  # .exe and .msi
+    unix_exe_magics = (b"#!", b"\x7FELF")  # Shebang and ELF
     if globals.os is Os.Windows:
         with exe.open("rb") as f:
-            exe_magic = f.read(8).startswith((b"MZ", b"ZM"))
+            exe_magic = f.read(8).startswith(windows_exe_magics)
         if exe_magic:
+            # Run as executable and get PID
             try:
                 return await asyncio.create_subprocess_exec(
                     str(exe),
@@ -210,8 +213,7 @@ async def _launch_exe(executable: str):
         mode = exe.stat().st_mode
         exe_flag = not (mode & stat.S_IEXEC < stat.S_IEXEC)
         with exe.open("rb") as f:
-            # Check for shebang, exe and msi magic numbers
-            exe_magic = f.read(8).startswith((b"#!", b"\x7FELF", b"MZ", b"ZM", b"\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1"))
+            exe_magic = f.read(8).startswith((*unix_exe_magics, *windows_exe_magics))
         if exe_magic and not exe_flag:
             # Should be executable but isn't, fix it
             exe.chmod(mode | stat.S_IEXEC)
@@ -224,7 +226,7 @@ async def _launch_exe(executable: str):
                     if mode & stat.S_IEXEC < stat.S_IEXEC:
                         file.chmod(mode | stat.S_IEXEC)
         if exe_magic and exe_flag:
-            # Run as executable
+            # Run as executable and get PID
             return await asyncio.create_subprocess_exec(
                 str(exe),
                 cwd=str(exe.parent),
